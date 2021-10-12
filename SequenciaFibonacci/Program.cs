0151075b4fa5fc6dc14e08c0552ef83c7b128731
brthema072 <brunomarthe@hotmail.com>
2021-10-12 00:20:13 -0300

    First commit

---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SequenciaFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.GenerateFibonacciSequence();
+        }
+
+        public void GenerateFibonacciSequence()
+        {
+            Console.WriteLine("Defina o tamanho da sequência");
+            string strSequenceLength = Console.ReadLine();
+            Console.WriteLine();
+
+            short.TryParse(strSequenceLength, out short sequenceLength);
+
+            int num1 = 0, num2 = 1, aux;
+
+            if (sequenceLength > 0)
+            {
+                int[] numbers = new int[sequenceLength];
+
+                for (short i = 0; i < sequenceLength; i++)
+                {
+                    if(i == 0)
+                    {
+                        numbers[i] = 0;
+                    }
+                    else
+                    {
+                        aux = num1;
+                        num1 = num2;
+                        num2 = num1 + aux;
+                        numbers[i] = num2;
+                    }
+
+                }
+
+                foreach (var number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+        }
+    }
+}
